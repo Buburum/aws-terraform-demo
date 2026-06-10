b5b5730f47c40f79d82e91e1e4a20698f3465d4f
Buburum <just.alexandr.kubasov@gmail.com>
2026-06-10 12:03:31 +0300

    Add project_name variable

--- variables.tf
+++ variables.tf
@@ -15,3 +15,9 @@ variable "env_name" {
   type        = string
   default     = "dev"
 }
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "demo"
+}
